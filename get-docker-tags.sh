4a73b241d9229e0dadaed4229a23335d72e95729
Eric Menendez <ericmenendez@gmail.com>
2016-12-15 08:08:28 -0800

    Dockerfile and Travis build process

---
+++ get-docker-tags.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+# Don't tag PR builds at all
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
+    # Tag with current git commit
+    printf -- "-t $1:${TRAVIS_COMMIT} "
+    # If master, tag with 'latest'
+    if [ "${TRAVIS_BRANCH}" = "master" ]; then
+        printf -- "-t $1:latest "
+    fi
+fi
