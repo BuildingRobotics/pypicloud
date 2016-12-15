d55dfc5797f18b27ba94693f84690f121bce9b25
Eric Menendez <ericmenendez@gmail.com>
2016-12-14 17:28:53 -0800

    printf --

--- get-docker-tags.sh
+++ get-docker-tags.sh
@@ -4,9 +4,9 @@ set -e
 # Don't tag PR builds at all
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     # Tag with current git commit
-    printf "-t $1:${TRAVIS_COMMIT} "
+    printf -- "-t $1:${TRAVIS_COMMIT} "
     # If master, tag with 'latest'
     if [ "${TRAVIS_BRANCH}" = "master" ]; then
-        printf "-t $1:latest "
+        printf -- "-t $1:latest "
     fi
 fi
